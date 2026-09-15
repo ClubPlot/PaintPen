460b8669c70126809807b18a4f87aaa3e1c5f725
William West <occidens@gmail.com>
2026-09-15 12:15:57 -0400

    Stub out track module

---
+++ src/track.ts
@@ -0,0 +1,13 @@
+type Point = [x: number, y: number];
+
+export function startPoint(): Point {
+
+}
+
+export function onTrack(pos: Point): boolean {
+
+}
+
+export function finished(pos: Point): boolean {
+
+}
